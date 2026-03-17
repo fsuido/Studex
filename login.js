7cd99cc2ac73fd19d2735838b117ae18a796023b
fsuido <116603633+fsuido@users.noreply.github.com>
2026-03-17 18:54:07 -0500

    Update login.js

--- login.js
+++ login.js
@@ -1,4 +1,5 @@
-const API = 'http://localhost:3000/api';
+// const API = 'http://localhost:3000/api';
+const API = 'https://studex-backend-vgk5.onrender.com/api';
 
 async function login() {
   const usuario = document.getElementById("usuario").value;
